bacdcab126b77d1bf65e3a59004368c0d1c00717
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-07-13 15:49:55 +0100

    can store in local storage

--- spec/noteStorageSpec.js
+++ spec/noteStorageSpec.js
@@ -1,11 +1,23 @@
 describe('noteStorage', () => {
 
-  it('can store the input element', () =>{
+  // it('can store the input element', () =>{
 
-    let element = "element double"
-    noteStorage = new NoteStorage(element);
-    expect(noteStorage.element).toBeTruthy();
-  })
+  //   let element = "element double"
+  //   noteStorage = new NoteStorage(element);
+  //   expect(noteStorage.element).toBeTruthy();
+  // })
 
+  it('can store notes in local storage', () =>{
+    noteStorage = new NoteStorage();
+    noteStorage.create("This is a test, this is a test");
+    let value = localStorage.getItem("This is a test, this");
+    expect(value).toEqual("This is a test, this is a test");
+  });
+
+  it('can create text keys', () => {
+    noteStorage = new NoteStorage();
+    let testText = noteStorage.getKey("HelloWorldHelloWorld12345")
+    expect(testText).toEqual('HelloWorldHelloWorld')
+  });
 
 });

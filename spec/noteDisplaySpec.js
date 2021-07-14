b1f49b81b58043c54798fc30a97489cf84e8c318
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-07-14 15:02:41 +0100

    finished refactor of interface

--- spec/noteDisplaySpec.js
+++ spec/noteDisplaySpec.js
@@ -1,13 +1,17 @@
 'use strict';
 
 describe('noteDisplay', () => {
-  localStorage.clear();
-  localStorage.setItem("1", "Test One");
-  let noteDisplay = new NoteDisplay();
-  let element = document.querySelector('#test-lsOutput');
-  let key = localStorage.key(0);
 
-  noteDisplay.all(element);
-  expect(element.innerHTML).toEqual(`<a href="#${key}">Test One</a><br>`);
-  localStorage.clear();
+  it('can create a list of stored notes', () => {
+    localStorage.clear();
+    localStorage.setItem("1", "Test One");
+    let noteDisplay = new NoteDisplay();
+    let element = document.querySelector('#test-lsOutput');
+    let key = localStorage.key(0);
+  
+    noteDisplay.all(element);
+    expect(element.innerHTML).toEqual(`<a href="#${key}">Test One</a><br>`);
+    localStorage.clear();
+  });
+
 });

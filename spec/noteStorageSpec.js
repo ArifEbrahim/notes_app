c2fd3fda17a7cb748644464e52bdec8399ba16c0
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-07-15 17:35:15 +0100

    refactored delete to class

--- spec/noteStorageSpec.js
+++ spec/noteStorageSpec.js
@@ -6,10 +6,23 @@ describe('noteStorage', () => {
     localStorage.clear();
     let noteStorage = new NoteStorage();
     noteStorage.create("This is a test, this is a test");
-    let key = localStorage.key(0)
-    let value = localStorage.getItem(key)
+    let key = localStorage.key(0);
+    let value = localStorage.getItem(key);
     expect(value).toEqual("This is a test, this is a test");
     localStorage.clear();
   });
+
+  it('can delete notes in local storage', () =>{
+    localStorage.clear();
+    let noteStorage = new NoteStorage();
+    noteStorage.create("This is a test, this is a test");
+    let key = localStorage.key(0);
+    noteStorage.delete(key);
+    expect(localStorage.length).toEqual(0);
+    localStorage.clear();
+  });
+
+
+
 
 });
